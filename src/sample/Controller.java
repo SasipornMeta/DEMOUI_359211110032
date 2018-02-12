786be39ca79b7b8a813ea76afcaca0cac5f8ae5e
Sasiporn <Sasiporn.409@gmail.com>
2018-02-12 16:51:33 +0700

    User Login with sqlite

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,77 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    private loginModel loginmodel = new loginModel();
+
+    @FXML
+    private Label dbStatus;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button btnLogin;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        if (this.loginmodel.isDatabaseConnection()) {
+            this.dbStatus.setText("Connected to DB");
+        } else {
+            this.dbStatus.setText("Not Connected to DB");
+        }
+    }   // initialize
+
+    @FXML
+    public void Login(ActionEvent event) {
+        try {
+            if (this.loginmodel.isLogin(this.username.getText(),
+                    this.password.getText())) {
+                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+                stage.close();
+
+                adminLogin();
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Your username or " +
+                        "password is not correxted.");
+            }
+        } catch (Exception localExceptin) {
+            localExceptin.printStackTrace();
+        }
+
+
+    }   // Login
+
+    private void adminLogin() {
+        try {
+            Stage adminStage = new Stage();
+            FXMLLoader adminLoader = new FXMLLoader();
+            Pane adminRoot = adminLoader.load(getClass().
+                    getResource("/Admin/AdminDashBoard.fxml").openStream());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("School Management System");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 500, 400));
         primaryStage.setResizable(false);                           // คำสั่งนี้ ทำให้ขนาด UI ขยายไม่ได้
         primaryStage.show();
     }
